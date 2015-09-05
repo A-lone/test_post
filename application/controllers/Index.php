1e65bceecc21640499334d3670cb5eea25975e05
A-lone <aloooomail@gmail.com>
2015-09-05 11:19:35 +0800

    js

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -3,4 +3,9 @@ class IndexController extends Yaf_Controller_Abstract {
     public function indexAction() {
         $this->getView();
     }
+    public function submitAction() {
+        $retArr = array('errorno'=>0, 'message'=>'sucess', 'data'=>$_POST);
+        echo json_encode($retArr);
+        exit(0);
+    }
 }
